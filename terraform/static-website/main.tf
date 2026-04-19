07a4fea978cf9b83ed18c957eaee943b7f8cfa26
b-zago <bart.zagozdon@gmail.com>
2026-04-20 01:41:55 +0200

    modules not needed rn

--- terraform/static-website/main.tf
+++ terraform/static-website/main.tf
@@ -1,22 +1,50 @@
-module "static_website_bucket" {
-  source      = "../modules/s3"
-  bucket_name = "${var.project_name}-bucket"
-  bucket_tags = { Project = "static_website" }
+resource "aws_s3_bucket" "aws_static_website_bucket" {
+  bucket = "${local.project_name}-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_pab" {
+  bucket = aws_s3_bucket.aws_static_website_bucket.id
+  #only via bucket policy
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_read_get_access" {
+  bucket = aws_s3_bucket.aws_static_website_bucket.id
+  policy = data.aws_iam_policy_document.allow_read_get_access.json
+}
+
+data "aws_iam_policy_document" "allow_read_get_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    effect = "Allow"
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.aws_static_website_bucket.arn}/*"]
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = module.static_website_bucket.bucket_id
+  bucket = aws_s3_bucket.aws_static_website_bucket.id
 
   index_document {
-    suffix = var.suffix
+    suffix = local.suffix
   }
 }
 
 resource "aws_s3_object" "website" {
-  bucket       = module.static_website_bucket.bucket_id
-  key          = "index.html"
-  source       = "./website/index.html"
-  content_type = "text/html"
+  for_each     = toset(["index.html", "app.js", "styles.css"])
+  bucket       = aws_s3_bucket.aws_static_website_bucket.id
+  key          = each.key
+  source       = "./website/${each.key}"
+  content_type = local.content_types[each.key]
 
-  etag = filemd5("./website/index.html")
+  etag = filemd5("./website/${each.key}")
 }
+
